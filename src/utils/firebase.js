88d0c52afdb950cf2041c32c8a4cb5d012032b6c
hsynarslan <hsyn06arslan@gmail.com>
2021-12-24 19:49:34 +0300

    correction

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -15,138 +15,66 @@ const firebaseConfig = {
 
 export const firebase = initializeApp(firebaseConfig);
 
-// //!-------------------------------
-// //* -----------ADD DATA ----------
-// //!-------------------------------
-// export const addInfo = info => {
-//   const db = getDatabase();
-//   const userRef = ref(db, 'contact'); //! realtime database olarak oluşturduğumuz database ismini girdik
-//   const newUserRef = push(userRef);
-//   set(newUserRef, {
-//     username: info.username,
-//     phoneNumber: info.phoneNumber,
-//     gender: info.gender
-//   });
-//   successNote('Successfully added!');
-//   // console.log('veri eklendi');
-// };
-
-// //!------------------------------
-// //*---------GET DATA ------------
-// //!-----------------------------
-// export const useFetch = () => {
-//   const [contactList, setContactList] = useState();
-
-//   const [isLoading, setIsLoading] = useState();
-
-//   useEffect(() => {
-//     setIsLoading(true);
-//     const db = getDatabase();
-//     const userRef = ref(db, 'contact'); //! realtime database olarak oluşturduğumuz database ismini girdik
-
-//     onValue(query(userRef), snapshot => {
-//       const contacts = snapshot.val(); //! veri snapshot ile gezip val() ile değerini alıyoruz
-
-//       const contactArray = []; //! boş bir array oluşturuyoruz ve verileri bunun içine atıyoruz daha sonra setContactList içerisine atıyoruz.
-
-//       for (let id in contacts) {
-//         contactArray.push({ id, ...contacts[id] }); //! contacts içerisindeki id'leri gez ve contacts[id]'leri array'a push et
-//       }
-
-//       setContactList(contactArray);
-//       setIsLoading(false);
-//     });
-//   }, []);
-//   // console.log(contactList);
-//   return { isLoading, contactList }; //! burada table'a gönderiyoruz
-// };
-
-// //!-----------------------------------
-// //* -----------DELETE DATA ------------
-// //!-----------------------------------
-// export const deleteInfo = id => {
-//   console.log(id);
-//   const db = getDatabase();
-
-//   remove(ref(db, 'contact/' + id));
-//   successNote('Successfully deleted!');
-// };
-
-// //!--------------------------------
-// //*---------UPDATE DATA -----------
-// //!--------------------------------
-// export const updateInfo = info => {
-//   // console.log(info);
-//   // const { id, username, phoneNumber, gender } = info;
-//   // console.log(id);
-//   // console.log(username);
-//   // console.log(phoneNumber);
-//   // console.log(gender);
-//   const db = getDatabase();
-
-//   // const infoData = {
-//   //   id: info.id,
-//   //   username: info.username,
-//   //   phoneNumber: info.phoneNumber,
-//   //   gender: info.gender
-//   // };
-
-//   const newUserKey = push(child(ref(db), 'contact')).key; //! yeni bir key üretiyoruz.
-
-//   console.log(newUserKey);
-
-//   const updates = {};
-//   updates['/contact/' + newUserKey] = info;
-
-//   // console.log(updates);
-//   // console.log(info);
-
-//   return update(ref(db), updates);
-// };
-
+//!-------------------------------
+//* -----------ADD DATA ----------
+//!-------------------------------
 export const addInfo = info => {
   const db = getDatabase();
-  const userRef = ref(db, 'contact');
+  const userRef = ref(db, 'contact'); //! realtime database olarak oluşturduğumuz database ismini girdik
   const newUserRef = push(userRef);
   set(newUserRef, {
     username: info.username,
     phoneNumber: info.phoneNumber,
     gender: info.gender
   });
-  successNote('Added successfully');
+  successNote('Successfully added!');
+  // console.log('veri eklendi');
 };
 
+//!------------------------------
+//*---------GET DATA ------------
+//!-----------------------------
 export const useFetch = () => {
   const [contactList, setContactList] = useState();
-  const [isLoading, setIsLoading] = useState(false);
+
+  const [isLoading, setIsLoading] = useState();
+
   useEffect(() => {
     setIsLoading(true);
-
     const db = getDatabase();
-    const userRef = ref(db, 'contact');
+    const userRef = ref(db, 'contact'); //! realtime database olarak oluşturduğumuz database ismini girdik
 
     onValue(query(userRef), snapshot => {
-      const contacts = snapshot.val();
-      // send an array of the values in database
-      const contactArray = [];
+      const contacts = snapshot.val(); //! veri snapshot ile gezip val() ile değerini alıyoruz
+
+      const contactArray = []; //! boş bir array oluşturuyoruz ve verileri bunun içine atıyoruz daha sonra setContactList içerisine atıyoruz.
+
       for (let id in contacts) {
-        // console.log(id);
-        contactArray.push({ id, ...contacts[id] });
+        contactArray.push({ id, ...contacts[id] }); //! contacts içerisindeki id'leri gez ve contacts[id]'leri array'a push et
       }
+
       setContactList(contactArray);
       setIsLoading(false);
     });
   }, []);
-  return { isLoading, contactList };
+  // console.log(contactList);
+  return { isLoading, contactList }; //! burada table'a gönderiyoruz
 };
 
+//!-----------------------------------
+//* -----------DELETE DATA ------------
+//!-----------------------------------
 export const deleteInfo = id => {
+  // console.log(id);
   const db = getDatabase();
-  // const userRef = ref(db, 'contact');
+
   remove(ref(db, 'contact/' + id));
-  successNote('Deleted');
+  successNote('Successfully deleted!');
 };
 
+//!--------------------------------
+//*---------UPDATE DATA -----------
+//!--------------------------------
 export const updateInfo = (id, username, phoneNumber, gender) => {
   const db = getDatabase();
 
